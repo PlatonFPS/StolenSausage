39f96d14b237ea4d9f877c076e432740879b5f5c
PlatonFPS <PlatonFPS@yandex.ru>
2023-11-11 10:53:26 +0300

    Added object drag by toggle click

---
+++ Assets/Scripts/MouseMove.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+    private void Awake()
+    {
+        //PlayerPrefs.SetInt("ToggleDrag", 1);
+        //PlayerPrefs.Save();
+    }
+
+    private Transform holdObject = null;
+
+    void Update()
+    {
+        if(PlayerPrefs.GetInt("ToggleDrag") == 1)
+        {
+            ToggleDrag();
+        }
+        if(holdObject != null)
+        {
+            MoveObject();
+        }
+    }
+
+    void MoveObject()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = holdObject.position.z;
+        holdObject.transform.position = mousePosition;
+    }
+
+    void ToggleDrag()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (holdObject == null)
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+                if (targetObject != null && targetObject.gameObject.layer == 6)
+                {
+                    holdObject = targetObject.transform;
+                }
+            }
+            else
+            {
+                holdObject = null;
+            }
+        }
+    }
+}
